47ae9b0983e6e22b646398a9476f4536ce32c39d
Meredith Wang <merwang@deloitte.com>
2023-05-17 12:39:45 -0500

    added bokeh visual

--- Home.py
+++ Home.py
@@ -74,7 +74,6 @@ with tab1:
                 # storing them into dataframe
                 currency_df = pd.DataFrame(df['currency_key'].value_counts().head(10))
                 currency_df['Currency Key'] = currency_df.index
-                print(currency_df)
                 fig = px.bar(currency_df, x='Currency Key', y='currency_key', color='currency_key', color_continuous_scale='Purp',
                              labels={
                                  'currency_key': 'Count'
@@ -227,7 +226,7 @@ with tab1:
         label['Label'] = label.index.map({'regular': 'Regular',
                                              'local': 'Local (Anomaly)',
                                              'global': 'Global (Anomaly)'})
-        print(label)
+
         fig = px.bar(label, x='Label', y='label', color = 'Label', color_discrete_map={'Regular':'#3C567F', 'Local (Anomaly)': '#dfaeff',
                                                                                                             'Global (Anomaly)': "#f3e2fe"},
                      # title='Distribution of Data Labels',
@@ -355,6 +354,204 @@ with tab2:
                  "We pick K at the spot where SSE starts to flatten out and forming an elbow."
                  "\n"
                  "In this case, we select K=3 as our cluster number based on the 'elbow' as indicated above.")
+
+    sub_title = '<p style=" color:#dfaeff; font-size: 25px"><b>Interactive Visualization to Select Suppliers</b></p>'
+    st.markdown(sub_title, unsafe_allow_html=True)
+
+
+    from bokeh.plotting import output_notebook
+    output_notebook()  # set default; alternative is output_file()
+
+    from bokeh.io import output_notebook  # prevent opening separate tab with graph
+
+
+    # arranging bokeh layout objects
+
+    from bokeh.layouts import row
+    from bokeh.layouts import grid
+
+    # ColumnDataSource for mapping between column name and list of data & CustomJS callbacks
+    from bokeh.models import CustomJS, ColumnDataSource
+    from bokeh.models import Button  # for saving data
+    from bokeh.events import ButtonClick  # for saving data
+
+    from bokeh.models.widgets import DataTable, DateFormatter, TableColumn
+    from bokeh.models import HoverTool
+    from bokeh.plotting import figure
+    from bokeh.models import Legend, LegendItem
+
+    # reading the data to variables
+    df = pd.read_csv('bokeh.csv')
+    x = df['sum'].to_list()
+    y = df['count'].to_list()
+    z = df['num_manual_edit'].to_list()
+    d = df['Supplier_ID'].to_list()
+    f = df['cause'].to_list()
+
+    colormap = {0: 'red', 1: 'green', 2: 'blue'}
+    colors = [colormap[x] for x in df['clusters']]
+    e = colors
+
+    # create first subplot
+    plot_width = 550
+    plot_height = 600
+
+    s1 = ColumnDataSource(data=dict(x=x, y=y, z=z, d=d, e=e, f=f))
+
+    fig01 = figure(
+        width=plot_width,
+        height=plot_height,
+        tools=["lasso_select", "reset", "save"],
+        title="Select Interesting Suppliers in this Plot",
+    )
+
+    fig01.circle("x", "y", source=s1, alpha=0.9, color="e")
+
+    # color legend
+    rc = fig01.rect(x=0, y=0, height=1, width=1, color=["red", "blue", "green"])
+    rc.visible = False
+
+    # let us define Cluster information and use it as a legend for the first plot
+    cluster_name = ["Cluster_0: profit= low, interaction: low, no of edits: 0",
+                    "Cluster_1: profit= medium, interaction: medium, no of edits: 0",
+                    "Cluster_2: profit= high, interaction: high, no of edits: 0,1"]
+    legend = Legend(items=[
+        LegendItem(label=cluster_name[i], renderers=[rc], index=i) for i in range(0, len(cluster_name))
+    ], location="top_left")
+    fig01.add_layout(legend, 'below')
+
+    # let us create an HoverTool and define what needs to be displayed
+    tooltips = [
+        ("Profit_value:", "@x"),
+        ("Frequency_of_interaction:", "@y"),
+        ("Number_of_Manual_Edits", "@z"),
+        ("Supplier_Id", "@d"),
+        ("Cause", "@f"),
+    ]
+
+    fig01.add_tools(HoverTool(tooltips=tooltips))
+
+
+    # create second subplot
+    s2 = ColumnDataSource(data=dict(x=[], y=[], z=[], d=[], e=[], f=[]))
+
+    fig02 = figure(
+        width=500,
+        height=600,
+        x_range=(0, 13),
+        y_range=(0, 10),
+        tools=["box_zoom", "wheel_zoom", "reset", "save"],
+        title="These are your selected Suppliers",
+    )
+
+    fig02.circle("x", "y", source=s2, alpha=0.9, color="e")
+
+    # let us create a dynamic table
+    columns = [
+        TableColumn(field="x", title="Profit value"),
+        TableColumn(field="y", title="Frequency of Interactions"),
+        TableColumn(field="z", title="Num_Edit"),
+        TableColumn(field="d", title="Supplier_Id"),
+        TableColumn(field="f", title="Cause"),
+    ]
+
+    table = DataTable(
+        source=s2,
+        columns=columns,
+        width=600,
+        height=150,
+        sortable=True,
+        selectable=True,
+        editable=True,
+    )
+    # Inspired from online Bokeh tutorials
+
+    # let us link subplots using CustomJS
+
+    s1.selected.js_on_change(
+        "indices",
+        CustomJS(
+            args=dict(s1=s1, s2=s2, table=table),
+            code="""
+            var inds = cb_obj.indices;
+            var d1 = s1.data;
+            var d2 = s2.data;
+            d2['x'] = []
+            d2['y'] = []
+            d2['z'] = []
+            d2['d'] = []
+            d2['e'] = []
+            d2['f'] = []
+            for (var i = 0; i < inds.length; i++) {
+                d2['x'].push(d1['x'][inds[i]])
+                d2['y'].push(d1['y'][inds[i]])
+                d2['z'].push(d1['z'][inds[i]])
+                d2['d'].push(d1['d'][inds[i]])
+                d2['e'].push(d1['e'][inds[i]])
+                d2['f'].push(d1['f'][inds[i]])
+            }
+            s2.change.emit();
+            table.change.emit();
+
+            var inds = source_data.selected.indices;
+            var data = source_data.data;
+            var out = "x, y, z, d, e, f \\n";
+            for (i = 0; i < inds.length; i++) {
+                out += data['x'][inds[i]] + "," + data['y'][inds[i]] + "," + data['z'][inds[i]]+ "," + data['d'][inds[i]] + "," + data['f'][inds[i]] + "\\n";
+            }
+            var file = new Blob([out], {type: 'text/plain'});
+
+        """,
+        ),
+    )
+
+
+    # let us create our save button
+
+    savebutton = Button(label="Save", button_type="success")
+    savebutton.js_on_event(ButtonClick, CustomJS(
+        args=dict(source_data=s1),
+        code="""
+            var inds = source_data.selected.indices;
+            var data = source_data.data;
+            var out = "Profit_value, Frequency_of_interaction, Number_of_Manual_Edits, Supplier_Id, Cause \\n";
+            for (var i = 0; i < inds.length; i++) {
+                out += data['x'][inds[i]] + "," + data['y'][inds[i]] + "," + data['z'][inds[i]] + "," + data['d'][inds[i]] + "," + data['f'][inds[i]] + "\\n";
+            }
+            var file = new Blob([out], {type: 'text/plain'});
+            var elem = window.document.createElement('a');
+            elem.href = window.URL.createObjectURL(file);
+            elem.download = 'selected-data.txt';
+            document.body.appendChild(elem);
+            elem.click();
+            document.body.removeChild(elem);
+            """
+    )
+                           )
+
+    # let us create an HoverTool and define what needs to be displayed
+    tooltips = [
+        ("Profit_value:", "@x"),
+        ("Frequency_of_interaction:", "@y"),
+        ("Number_of_Manual_Edits", "@z"),
+        ("Supplier_Id", "@d"),
+        ("Cause", "@f"),
+    ]
+
+    fig02.add_tools(HoverTool(tooltips=tooltips))
+
+    # let us create a layout and display all our objects together
+    layout = grid([fig01, fig02, table, savebutton], ncols=2)
+    st.bokeh_chart(layout)
+    output_notebook()
+
+
+
+
+
+
+
+
 
 css = '''
 <style>
